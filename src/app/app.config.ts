24f93564d81610e6fbfaff5c486b55372f062072
Kostya <crraay@gmail.com>
2024-08-16 12:03:35 +0700

    feat: display api response, add environment props

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, enableProdMode, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { ApiModule } from './web-api/api.module';
+import { environment } from 'environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(ApiModule.forRoot({ rootUrl: environment.apiUrl }))
+  ]
 };
--- src/app/pages/edit-habits/edit-habits.component.ts
+++ src/app/pages/edit-habits/edit-habits.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { HabitResponse } from '@webapi/models';
+import { HabitService } from '@webapi/services';
 
 @Component({
   selector: 'app-edit-habits',
   standalone: true,
-  imports: [],
+  imports: [
+    JsonPipe
+  ],
   templateUrl: './edit-habits.component.html',
   styleUrl: './edit-habits.component.scss'
 })
-export class EditHabitsComponent {
+export class EditHabitsComponent implements OnInit {
+
+  data: HabitResponse[];
+
+  constructor(
+    private habitService: HabitService
+  ) { }
+  
+  ngOnInit(): void {
+    this.habitService.getHabits()
+      .subscribe(response => {
+        this.data = response;
+      });
+  }
 
 }
